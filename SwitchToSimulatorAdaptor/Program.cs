f5a79a579d9ad53ce12d5dce8bf409aea7291f4f
longly <mrTangFg@163.com>
2026-02-03 11:04:19 +0800

    [A] 增加接口查看函数

--- SwitchToSimulatorAdaptor/Program.cs
+++ SwitchToSimulatorAdaptor/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 
 using SwitchToSimulatorAdaptor;
+using SwitchToSimulatorAdaptor.SharpPcap;
+
+// SharpPcapManager.PrintDevicesList(); // 查看所有接口
 
 var entry = new AdaptorEntry();
 entry.Start();
--- SwitchToSimulatorAdaptor/SharpPcap/SharpPcapManager.cs
+++ SwitchToSimulatorAdaptor/SharpPcap/SharpPcapManager.cs
@@ -99,6 +99,38 @@ public class SharpPcapManager : IDisposable
         Logger.Instance?.LogError(e.Message);
     }
     
+    public static void PrintDevicesList()
+    {
+        //获取所有网络设备
+        var devices = CaptureDeviceList.Instance;
+        
+        Console.WriteLine($"找到 {devices.Count} 个网络接口 \n");
+
+        for (int i = 0; i < devices.Count; i++)
+        {
+            var device = devices[i];
+            
+            Console.WriteLine($"设备[{ i }]：{ device.Name }");
+            Console.WriteLine($"     描述：{ device.Description }");
+
+            if (device is LibPcapLiveDevice libPcapDevice)
+            {
+                Console.WriteLine($"     友好名称：{ libPcapDevice.Interface?.FriendlyName }");
+                
+                //打印 Ip 地址
+                foreach (var addr in libPcapDevice.Interface?. Addresses ?? [])
+                {
+                    if (addr.Addr?.ipAddress != null)
+                    {
+                        Console.WriteLine($"     IP:{ addr.Addr?.ipAddress }");
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine();
+    }
+    
     public void Dispose()
     {
         _packetSender?.OnLog -= OnPacketSenderLogEvent;
